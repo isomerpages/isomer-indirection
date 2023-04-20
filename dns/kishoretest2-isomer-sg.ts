b1b1eb881aa9a12476c5e0213eb79349ababbcec
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-04-20 14:33:57 +0800

    Create kishoretest2-isomer-sg.ts

--- dns/kishoretest2-isomer-sg.ts
+++ dns/kishoretest2-isomer-sg.ts
@@ -1,8 +1,8 @@
 import { Record } from "@pulumi/cloudflare";
 
 export const createRecords = (zoneId: string): Record[] => {
-  const record1 = new Record("kishoreindirectiontest3", {
-    name: "kishoreindirectiontest3.isomer.gov.sg",
+  const record1 = new Record("kishoreindirectiontest10", {
+    name: "kishoreindirectiontest10.isomer.gov.sg",
     type: "CNAME",
     ttl: 120,
     zoneId: zoneId,
