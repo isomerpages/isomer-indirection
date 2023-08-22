148371cdd9a77fa3f7a74f43a8201d271740ae8a
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-08-22 10:57:50 +0800

    feat: introduce AWS Route 53 as indirection layer

---
+++ cloudflare.ts
@@ -0,0 +1,68 @@
+import { Record } from "@pulumi/cloudflare";
+import { env } from "process";
+
+const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("hostedon-ns1", {
+      name: "hostedon",
+      type: "NS",
+      ttl: 60,
+      zoneId: zoneId,
+      proxied: false,
+      value: "ns-1053.awsdns-03.org",
+    }),
+
+    new Record("hostedon-ns2", {
+      name: "hostedon",
+      type: "NS",
+      ttl: 60,
+      zoneId: zoneId,
+      proxied: false,
+      value: "ns-641.awsdns-16.net",
+    }),
+
+    new Record("hostedon-ns3", {
+      name: "hostedon",
+      type: "NS",
+      ttl: 60,
+      zoneId: zoneId,
+      proxied: false,
+      value: "ns-1615.awsdns-09.co.uk",
+    }),
+
+    new Record("hostedon-ns4", {
+      name: "hostedon",
+      type: "NS",
+      ttl: 60,
+      zoneId: zoneId,
+      proxied: false,
+      value: "ns-476.awsdns-59.com",
+    }),
+
+    new Record("hostedon-ds", {
+      name: "hostedon",
+      type: "DS",
+      ttl: 60,
+      zoneId: zoneId,
+      proxied: false,
+      data: {
+        keyTag: 5403,
+        algorithm: 13,
+        digestType: 2,
+        digest:
+          "AC93FBD6C960986A64A96F696837F934A5839F8E187A053B5EA50E7BA13E1615",
+      },
+    }),
+  ];
+
+  return records;
+};
+
+export async function createCloudflareRecords() {
+  const CLOUDFLARE_ZONE_ID = env.CLOUDFLARE_ZONE_ID;
+  if (!CLOUDFLARE_ZONE_ID) {
+    throw new Error("CLOUDFLARE_ZONE_ID must be set");
+  }
+
+  createRecords(CLOUDFLARE_ZONE_ID);
+}
--- createAllRecords.ts
+++ createAllRecords.ts
@@ -3,16 +3,16 @@ import * as path from "path";
 import { env } from "process";
 
 export async function createAllRecords() {
-  const CLOUDFLARE_ZONE_ID = env.CLOUDFLARE_ZONE_ID;
-  if (!CLOUDFLARE_ZONE_ID) {
-    throw new Error("CLOUDFLARE_ZONE_ID must be set");
+  const AWS_HOSTED_ZONE_ID = env.AWS_HOSTED_ZONE_ID;
+  if (!AWS_HOSTED_ZONE_ID) {
+    throw new Error("AWS_HOSTED_ZONE_ID must be set");
   }
   const DNS_DIR = "./dns";
   const files = fs.readdirSync(DNS_DIR);
   const promises = files.map(async (file) => {
     const modulePath = path.join(DNS_DIR, file);
     const { createRecords } = await import(`./${modulePath}`);
-    createRecords(CLOUDFLARE_ZONE_ID);
+    createRecords(AWS_HOSTED_ZONE_ID);
   });
 
   await Promise.all(promises);
