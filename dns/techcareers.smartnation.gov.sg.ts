58c916512d69902f9fa1618991b87f636530c37d
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-09-05 10:52:02 +0800

    Create techcareers.smartnation.gov.sg.ts

---
+++ dns/techcareers.smartnation.gov.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("techcareers.smartnation.gov.sg A", {
+      name: "techcareers-smartnation-gov-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d12m8hwmtw2zom.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("techcareers.smartnation.gov.sg AAAA", {
+      name: "techcareers-smartnation-gov-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d12m8hwmtw2zom.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
