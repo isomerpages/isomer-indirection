acd80ec7312d1d661088e04e900cdb2b9ed8594f
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-09-05 10:23:19 +0800

    Create asean-ifce.com.ts

---
+++ dns/asean-ifce.com.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("asean-ifce.com A", {
+      name: "asean-ifce-com",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d3gt6ml5xd01gv.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("asean-ifce.com AAAA", {
+      name: "asean-ifce-com",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d3gt6ml5xd01gv.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
