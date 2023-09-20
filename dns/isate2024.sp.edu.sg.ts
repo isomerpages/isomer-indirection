2490dfd4f72d710e610c13a01e4ed96f686694b0
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-09-20 14:52:04 +0800

    Update isate2024.sp.edu.sg.ts

--- dns/isate2024.sp.edu.sg.ts
+++ dns/isate2024.sp.edu.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "dwisscd8exrrb.cloudfront.net",
+          name: "d1c9z7nhkp5txm.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "dwisscd8exrrb.cloudfront.net",
+          name: "d1c9z7nhkp5txm.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
