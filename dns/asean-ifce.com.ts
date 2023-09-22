320cb0d4f31cd929e89c7b2a7d2f03c50b8a1b4e
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-09-22 09:31:32 +0800

    Update asean-ifce.com.ts

--- dns/asean-ifce.com.ts
+++ dns/asean-ifce.com.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d16dewo7m8md6o.cloudfront.net",
+          name: "d3biczdulp58is.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d16dewo7m8md6o.cloudfront.net",
+          name: "d3biczdulp58is.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
