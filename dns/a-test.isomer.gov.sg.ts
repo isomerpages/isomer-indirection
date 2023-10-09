d46cd96902ae37c39ec446deb93003826512e24b
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-10-09 17:57:19 +0800

    Create a-test.isomer.gov.sg.ts

---
+++ dns/a-test.isomer.gov.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("a-test.isomer.gov.sg A", {
+      name: "a-test-isomer-gov-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d24qdm56hvb0hq.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("a-test.isomer.gov.sg AAAA", {
+      name: "a-test-isomer-gov-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d24qdm56hvb0hq.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
