7c23b05ccc7deeb0d5f9c73ceede97ef2010d064
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-10-17 10:18:12 +0800

    Create csro.gov.sg.ts

---
+++ dns/csro.gov.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("csro.gov.sg A", {
+      name: "csro-gov-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d1ax0hxtro6lmv.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("csro.gov.sg AAAA", {
+      name: "csro-gov-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d1ax0hxtro6lmv.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
