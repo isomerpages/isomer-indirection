1abb8c364ec9bbf93087acb3351db21a7601faf2
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-10-17 16:22:03 +0800

    Update facsmab.org.ts

--- dns/facsmab.org.ts
+++ dns/facsmab.org.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d4mpcrsc0v1pj.cloudfront.net",
+          name: "d17guw0ohkecxs.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d4mpcrsc0v1pj.cloudfront.net",
+          name: "d17guw0ohkecxs.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
