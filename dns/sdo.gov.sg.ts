144acd6a17b1155e20225c20864a73a61f0b3f64
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-10-17 10:17:42 +0800

    Update sdo.gov.sg.ts

--- dns/sdo.gov.sg.ts
+++ dns/sdo.gov.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "drxyspbftglfm.cloudfront.net",
+          name: "d1yonzlf864twn.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "drxyspbftglfm.cloudfront.net",
+          name: "d1yonzlf864twn.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
