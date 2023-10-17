fdc565fa4a519496403c75d744a65ddb69c3b191
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-10-17 10:17:46 +0800

    Create vital.gov.sg.ts

---
+++ dns/vital.gov.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("vital.gov.sg A", {
+      name: "vital-gov-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d2xsb0kqqg8zr3.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("vital.gov.sg AAAA", {
+      name: "vital-gov-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d2xsb0kqqg8zr3.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
