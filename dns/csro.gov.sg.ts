cc6ccdc3889ca3774ac88f285bf5a999f78e8b34
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-10-24 10:13:12 +0800

    Update csro.gov.sg.ts

--- dns/csro.gov.sg.ts
+++ dns/csro.gov.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d1ax0hxtro6lmv.cloudfront.net",
+          name: "d1f8gsjl3inc9s.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d1ax0hxtro6lmv.cloudfront.net",
+          name: "d1f8gsjl3inc9s.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
