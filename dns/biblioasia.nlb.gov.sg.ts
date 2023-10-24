dc517a82a1dd6d03e40be3a71a7de40418a7acad
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-10-24 09:37:08 +0800

    Create biblioasia.nlb.gov.sg.ts

---
+++ dns/biblioasia.nlb.gov.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("biblioasia.nlb.gov.sg A", {
+      name: "biblioasia-nlb-gov-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d28tcbr3qus6k6.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("biblioasia.nlb.gov.sg AAAA", {
+      name: "biblioasia-nlb-gov-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d28tcbr3qus6k6.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
