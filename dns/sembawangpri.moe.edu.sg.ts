4034a9c711cbc2c9b52d3ff7ecbeda2e912efe69
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-10-30 15:30:40 +0800

    Create sembawangpri.moe.edu.sg.ts

---
+++ dns/sembawangpri.moe.edu.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("sembawangpri.moe.edu.sg A", {
+      name: "sembawangpri-moe-edu-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d1cuu4vv4omzo.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("sembawangpri.moe.edu.sg AAAA", {
+      name: "sembawangpri-moe-edu-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d1cuu4vv4omzo.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
