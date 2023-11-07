f5b2c47ea4b9a759ac63f87f9f219bf6580fb1c5
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-11-07 18:06:22 +0800

    Create uptlc.moe.edu.sg.ts

---
+++ dns/uptlc.moe.edu.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("uptlc.moe.edu.sg A", {
+      name: "uptlc-moe-edu-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "dsso28ekksuun.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("uptlc.moe.edu.sg AAAA", {
+      name: "uptlc-moe-edu-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "dsso28ekksuun.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
