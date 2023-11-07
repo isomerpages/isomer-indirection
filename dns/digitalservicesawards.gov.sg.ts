0531936d44b2f349588d37e7616fdec261a059cd
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-11-07 17:42:56 +0800

    Create digitalservicesawards.gov.sg.ts

---
+++ dns/digitalservicesawards.gov.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("digitalservicesawards.gov.sg A", {
+      name: "digitalservicesawards-gov-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "dbjquh9g66gy6.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("digitalservicesawards.gov.sg AAAA", {
+      name: "digitalservicesawards-gov-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "dbjquh9g66gy6.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
