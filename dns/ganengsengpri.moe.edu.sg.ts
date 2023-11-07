aee397f3f00b38582518f76cac808d16d3e7af3c
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-11-07 17:58:36 +0800

    Create ganengsengpri.moe.edu.sg.ts

---
+++ dns/ganengsengpri.moe.edu.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("ganengsengpri.moe.edu.sg A", {
+      name: "ganengsengpri-moe-edu-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "dqib7jaf9jsel.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("ganengsengpri.moe.edu.sg AAAA", {
+      name: "ganengsengpri-moe-edu-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "dqib7jaf9jsel.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
