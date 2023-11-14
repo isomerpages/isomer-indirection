9a89672f5a4cd16366d1527ef604c711713b9d8c
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2023-11-14 10:27:55 +0800

    Create greenwoodpri.moe.edu.sg.ts

---
+++ dns/greenwoodpri.moe.edu.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("greenwoodpri.moe.edu.sg A", {
+      name: "greenwoodpri-moe-edu-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d1z6yvmw1xwqg4.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("greenwoodpri.moe.edu.sg AAAA", {
+      name: "greenwoodpri-moe-edu-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d1z6yvmw1xwqg4.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
