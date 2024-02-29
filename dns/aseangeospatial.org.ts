878ba7b08921161590e33b8ba745f645d7ec05ba
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2024-02-29 16:07:09 +0800

    Create aseangeospatial.org.ts

---
+++ dns/aseangeospatial.org.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("aseangeospatial.org A", {
+      name: "aseangeospatial-org",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "db83fvkrdy70i.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("aseangeospatial.org AAAA", {
+      name: "aseangeospatial-org",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "db83fvkrdy70i.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
