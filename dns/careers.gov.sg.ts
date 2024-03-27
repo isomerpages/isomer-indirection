9181f12975bc05e0a468ee45dd3f13812b28afe2
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2024-03-27 14:10:28 +0800

    Create careers.gov.sg.ts

---
+++ dns/careers.gov.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("careers.gov.sg A", {
+      name: "careers-gov-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d3ltswz8oqmr5v.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("careers.gov.sg AAAA", {
+      name: "careers-gov-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d3ltswz8oqmr5v.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
