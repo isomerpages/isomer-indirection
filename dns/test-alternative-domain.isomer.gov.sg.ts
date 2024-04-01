90e1241ece097fb0039954df6c27a645f7742269
Kishore <42832651+kishore03109@users.noreply.github.com>
2024-04-01 18:38:39 +0800

    test(sl): test alternative domain

--- dns/test-alternative-domain.isomer.gov.sg.ts
+++ dns/test-alternative-domain.isomer.gov.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d2sbbzcx1dbopg.cloudfront.net",
+          name: "d3lifq9tois2xt.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d2sbbzcx1dbopg.cloudfront.net",
+          name: "d3lifq9tois2xt.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
