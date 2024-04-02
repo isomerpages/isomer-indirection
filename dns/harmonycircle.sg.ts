9b09d73512642833ea6bbf97161536929dc77a4f
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2024-04-02 10:05:46 +0800

    Update harmonycircle.sg.ts

--- dns/harmonycircle.sg.ts
+++ dns/harmonycircle.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d3kb131amfgi44.cloudfront.net",
+          name: "d2oek2kmsisfok.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d3kb131amfgi44.cloudfront.net",
+          name: "d2oek2kmsisfok.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
