1d8811a1e1f04e2a76c824c16b183efc13142385
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2024-05-15 16:52:27 +0800

    Create vapt-ndp.isomer.gov.sg.ts

---
+++ dns/vapt-ndp.isomer.gov.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("vapt-ndp.isomer.gov.sg A", {
+      name: "vapt-ndp-isomer-gov-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d2kjbbcytvb26e.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("vapt-ndp.isomer.gov.sg AAAA", {
+      name: "vapt-ndp-isomer-gov-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d2kjbbcytvb26e.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
