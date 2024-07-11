e309fd1f2c0f22fc544f480d18ee3dde16eff0a2
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-07-11 15:04:43 +0800

    Update hci.edu.sg.ts

--- dns/hci.edu.sg.ts
+++ dns/hci.edu.sg.ts
@@ -28,6 +28,18 @@ export const createRecords = (zoneId: string): Record[] => {
         },
       ],
     }),
+
+    // NOTE: The following records are needed by HCI for their OpenCerts issuance verification
+    new Record("hci.edu.sg TXT", {
+      name: "hci-edu-sg",
+      type: "TXT",
+      zoneId: zoneId,
+      ttl: 3600,
+      records: [
+        "openatts net=ethereum netId=1 addr=0xDc45D2E1482093E6E8933688347969ba785C39f9",
+        "openatts a=dns-did; p=did:ethr:0x0ee7FeE3eC819CC838A162F4a8DA0204Fd5B5dB1#controller; v=1.0",
+      ],
+    }),
   ];
 
   return records;
