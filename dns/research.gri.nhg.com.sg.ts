cbcea69ee5c3115e7b59efe6639bdc998e9a813c
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2024-08-14 16:00:32 +0800

    Create research.gri.nhg.com.sg.ts

---
+++ dns/research.gri.nhg.com.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("research.gri.nhg.com.sg A", {
+      name: "research-gri-nhg-com-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d2lbrvvv5hjjjd.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("research.gri.nhg.com.sg AAAA", {
+      name: "research-gri-nhg-com-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d2lbrvvv5hjjjd.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
