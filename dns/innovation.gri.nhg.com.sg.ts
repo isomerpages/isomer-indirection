9f2bd60acb70deb77cd907a82fd86dccd148a276
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2024-08-14 16:00:58 +0800

    Create innovation.gri.nhg.com.sg.ts

---
+++ dns/innovation.gri.nhg.com.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("innovation.gri.nhg.com.sg A", {
+      name: "innovation-gri-nhg-com-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "db2zm16mm9wud.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("innovation.gri.nhg.com.sg AAAA", {
+      name: "innovation-gri-nhg-com-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "db2zm16mm9wud.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
