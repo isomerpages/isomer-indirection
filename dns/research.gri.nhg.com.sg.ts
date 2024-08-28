735c3b975c0f18f89df7feb842b3386b491e1775
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2024-08-28 20:12:02 +0800

    Update research.gri.nhg.com.sg.ts

--- dns/research.gri.nhg.com.sg.ts
+++ dns/research.gri.nhg.com.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d2lbrvvv5hjjjd.cloudfront.net",
+          name: "d2humxesvml68.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d2lbrvvv5hjjjd.cloudfront.net",
+          name: "d2humxesvml68.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
