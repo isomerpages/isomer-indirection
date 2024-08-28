cd02bcb388d686904ed8e71981de2e7aba7151a3
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2024-08-28 20:11:58 +0800

    Update talentdev.gri.nhg.com.sg.ts

--- dns/talentdev.gri.nhg.com.sg.ts
+++ dns/talentdev.gri.nhg.com.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "dszksos4s1ri8.cloudfront.net",
+          name: "d2p1j6yhl69j1g.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "dszksos4s1ri8.cloudfront.net",
+          name: "d2p1j6yhl69j1g.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
