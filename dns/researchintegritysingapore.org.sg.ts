e96af67c95e22a56a73b329c0478302a2c138e02
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2024-10-11 09:47:35 +0800

    Create researchintegritysingapore.org.sg.ts

---
+++ dns/researchintegritysingapore.org.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("researchintegritysingapore.org.sg A", {
+      name: "researchintegritysingapore-org-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d36xuk8shkvkqa.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("researchintegritysingapore.org.sg AAAA", {
+      name: "researchintegritysingapore-org-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d36xuk8shkvkqa.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
