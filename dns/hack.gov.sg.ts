7997161c13e6be8a88b6cb01dc1094f6c9520bbb
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2025-01-21 13:49:11 +0800

    Update hack.gov.sg.ts

--- dns/hack.gov.sg.ts
+++ dns/hack.gov.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d2glfngpirzfzh.cloudfront.net",
+          name: "d17vlmkzu63xec.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d2glfngpirzfzh.cloudfront.net",
+          name: "d17vlmkzu63xec.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
