412ef69f55f6d2d98b138fb461df427b92fe4609
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2025-01-23 08:40:59 +0800

    Create sentosacove.com.ts

---
+++ dns/sentosacove.com.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("sentosacove.com A", {
+      name: "sentosacove-com",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d2vrcok7066414.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("sentosacove.com AAAA", {
+      name: "sentosacove-com",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d2vrcok7066414.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
