d0e4b026e69adc4b98badd0cc0adb0538e031883
seaerchin <jiachin@open.gov.sg>
2025-02-05 14:50:03 +0800

    chore: add stb

---
+++ dns/stb.gov.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("stb.gov.sg A", {
+      name: "stb.gov.sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d17t8cestx11zn.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("stb.gov.sg AAAA", {
+      name: "stb.gov.sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d17t8cestx11zn.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
