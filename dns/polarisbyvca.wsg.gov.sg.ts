1b603d7b154d8b24769c69732cd74d15fd412c2d
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2025-02-28 10:31:28 +0800

    Create polarisbyvca.wsg.gov.sg.ts

---
+++ dns/polarisbyvca.wsg.gov.sg.ts
@@ -0,0 +1,34 @@
+import { Record } from "@pulumi/aws/route53";
+import { CLOUDFRONT_HOSTED_ZONE_ID } from "../constants";
+
+export const createRecords = (zoneId: string): Record[] => {
+  const records = [
+    new Record("polarisbyvca.wsg.gov.sg A", {
+      name: "polarisbyvca-wsg-gov-sg",
+      type: "A",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d13jhktiainhsy.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+
+    new Record("polarisbyvca.wsg.gov.sg AAAA", {
+      name: "polarisbyvca-wsg-gov-sg",
+      type: "AAAA",
+      zoneId: zoneId,
+      aliases: [
+        {
+          name: "d13jhktiainhsy.cloudfront.net",
+          zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
+          evaluateTargetHealth: false,
+        },
+      ],
+    }),
+  ];
+
+  return records;
+};
