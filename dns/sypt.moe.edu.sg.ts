8b9261814e446d68e5a3014375c3895e04075a07
Isomer Admin <60170571+isomeradmin@users.noreply.github.com>
2025-03-03 19:14:36 +0800

    Update sypt.moe.edu.sg.ts

--- dns/sypt.moe.edu.sg.ts
+++ dns/sypt.moe.edu.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "dd0fnkxc6mn3b.cloudfront.net",
+          name: "d14n9y9150up1d.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "dd0fnkxc6mn3b.cloudfront.net",
+          name: "d14n9y9150up1d.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
