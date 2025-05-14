4aa6896f45c2266a0da0975d5f6ac7dee3759101
seaerchin <jiachin@open.gov.sg>
2025-05-14 10:07:41 +0800

    Update mddi.gov.sg

--- dns/mddi.gov.sg.ts
+++ dns/mddi.gov.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d2wybfiyq61ebo.cloudfront.net",
+          name: "d1ublpm7lx4d2u.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d2wybfiyq61ebo.cloudfront.net",
+          name: "d1ublpm7lx4d2u.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
