56bfa0e6b1df5fdb41cf3a8aad7964ed15456556
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2025-05-26 11:05:54 +0800

    Update ndp.gov.sg.ts

--- dns/ndp.gov.sg.ts
+++ dns/ndp.gov.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d256d4e5nx1vj2.cloudfront.net",
+          name: "d3lq1v8ua4ym9l.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d256d4e5nx1vj2.cloudfront.net",
+          name: "d3lq1v8ua4ym9l.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
