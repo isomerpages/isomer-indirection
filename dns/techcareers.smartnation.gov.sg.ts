be40dd6d55f677f76af6e3cc63849f387e632e3f
seaerchin <jiachin@open.gov.sg>
2025-06-09 13:41:43 +0800

    chore: update techcareers

--- dns/techcareers.smartnation.gov.sg.ts
+++ dns/techcareers.smartnation.gov.sg.ts
@@ -9,7 +9,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d12m8hwmtw2zom.cloudfront.net",
+          name: "d2022u808auw7a.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
@@ -22,7 +22,7 @@ export const createRecords = (zoneId: string): Record[] => {
       zoneId: zoneId,
       aliases: [
         {
-          name: "d12m8hwmtw2zom.cloudfront.net",
+          name: "d2022u808auw7a.cloudfront.net",
           zoneId: CLOUDFRONT_HOSTED_ZONE_ID,
           evaluateTargetHealth: false,
         },
